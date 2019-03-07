b0669f825d54ad12d0abc45d59e5aefc5c34d343
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-03-07 15:14:06 +0100

    updated webpack-config, deleted unnecessary .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist/assets/js/"),
     filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
   }
 };
