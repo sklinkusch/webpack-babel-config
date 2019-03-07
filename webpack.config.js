43f43f26eaf389d2bf3478388944a7cedf86ea87
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-03-07 15:41:24 +0100

    added stuff for production version

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist/assets/js/"),
+    path: path.resolve(__dirname, "docs/"),
     filename: "main.js"
   },
   module: {
